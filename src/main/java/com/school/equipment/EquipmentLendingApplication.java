7f29a159b70b21fb9e086b36e7d0f456691f4357
2024tm93553 <2024tm93553@wilp.bits-pilani.ac.in>
2025-11-08 15:30:43 +0530

    Add configuration to connect to mysql db and application setup

---
+++ src/main/java/com/school/equipment/EquipmentLendingApplication.java
@@ -0,0 +1,11 @@
+package com.school.equipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentLendingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentLendingApplication.class, args);
+    }
+}
